ce91633b30e6eab7b839cca7c672cff0182ced16
Ranked2Hard <101624223+Ranked2Hard@users.noreply.github.com>
2022-06-24 09:21:47 +1000

    Update Loader.java

--- Loader.java
+++ Loader.java
@@ -6,13 +6,13 @@ public class loader extends JavaPlugin{
 	
 	public void onEnable() {
 		System.out.println("============[Loader]============ ");
-		System.out.println("Simple Loader By Funkyranveer    ");
+		System.out.println("Simple Loader By Ranked2Hard     ");
 		System.out.println("Enabled: True                    ");
 		System.out.println("============[Loader]============ ");
 	}
 	public void onDisable() {
 		System.out.println("============[Loader]============ ");
-		System.out.println("Simple Loader By Funkyranveer    ");
+		System.out.println("Simple Loader By Ranked2Hard     ");
 		System.out.println("Enabled: False - Contact Support ");
 		System.out.println("============[Loader]============ ");
 	}
