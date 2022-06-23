459cd87bb646ed49101d8819c21ec3d6accc94aa
Ranked2Hard <101624223+Ranked2Hard@users.noreply.github.com>
2022-06-24 09:06:07 +1000

    Create Loader.java

---
+++ Loader.java
@@ -0,0 +1,19 @@
+package me.funkycmd.java;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class loader extends JavaPlugin{
+	
+	public void onEnable() {
+		System.out.println("============[Loader]============ ");
+		System.out.println("Simple Loader By Funkyranveer    ");
+		System.out.println("Enabled: True                    ");
+		System.out.println("============[Loader]============ ");
+	}
+	public void onDisable() {
+		System.out.println("============[Loader]============ ");
+		System.out.println("Simple Loader By Funkyranveer    ");
+		System.out.println("Enabled: False - Contact Support ");
+		System.out.println("============[Loader]============ ");
+	}
+}
